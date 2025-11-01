157e03ed48862d0df9ab7d6b4d129ac359d818cc
Neil Ruiz <NeilRuizAmaya@gmail.com>
2025-10-31 19:49:07 -0500

    Eliminación de la importación del objeto React completo y de los comentarios asociados al uso de modales de la versión anterior de la App, en index.jsx de la carpeta CreateTodoButton

--- src/ui/CreateTodoButton/index.jsx
+++ src/ui/CreateTodoButton/index.jsx
@@ -1,10 +1,6 @@
-import React from 'react';
 import './CreateTodoButton.css';
 
 function CreateTodoButton(props) {
-  // const onClickButton = () => {
-  //   props.setOpenModal(prevState => !prevState);
-  // };
 
   return (
     <button
